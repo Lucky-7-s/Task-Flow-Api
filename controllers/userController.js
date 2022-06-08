3d6942597672eb7bdcad89ab34d8f209a19d446a
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 10:33:20 -0600

    add create user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,4 +10,11 @@ router.get('/', function (req, res) {
         .then((users) => res.status(200).json({ user: users }))
 });
 
+// POST create new username
+router.post('/', function (req, res) {
+    User.create(req.body).then((user) => {
+        res.status(201).json({ user: user })
+    });
+});
+
 module.exports = router;
