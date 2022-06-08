0d300737b661bab7a63ae9d0d8bfff749a9be070
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 10:29:35 -0600

    add GET all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,4 +3,11 @@ const User = require('../models/userSchema');
 
 const router = express.Router();
 
+// GET all users
+router.get('/', function (req, res) {
+    User.find()
+        .populate('tasks', ['tasks', 'status'])
+        .then((users) => res.status(200).json({ user: users }))
+});
+
 module.exports = router;
