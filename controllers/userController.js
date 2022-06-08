a8ef95d0a0a3b4aeb95f213aaaf83c71853722b6
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 10:52:14 -0600

    moved GET user by username above post

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,13 +10,6 @@ router.get('/', function (req, res) {
         .then((users) => res.status(200).json({ user: users }))
 });
 
-// POST create new username
-router.post('/', function (req, res) {
-    User.create(req.body).then((user) => {
-        res.status(201).json({ user: user })
-    });
-});
-
 // GET user by username
 router.get('/:user', function (req, res) {
     User.find({ username: req.params.user })
@@ -24,10 +17,17 @@ router.get('/:user', function (req, res) {
         .then((user) => res.status(200).json({ user: user }))
 });
 
+// POST create new username
+router.post('/', function (req, res) {
+    User.create(req.body).then((user) => {
+        res.status(201).json({ user: user })
+    });
+});
+
 // DELETE user by username
 router.delete('/username/:user', function (req, res) {
     User.findOneAndDelete({ username: req.params.user })
-    .then((user) => res.status(200).json({ user: user }));
+        .then((user) => res.status(200).json({ user: user }));
 });
 
 module.exports = router;
