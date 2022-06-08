cfd0d86ec219fd2b1f1a84d45f30823cdceffa32
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 10:36:28 -0600

    add GET all tasks route

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,4 +3,11 @@ const Task = require('../models/taskSchema');
 
 const router = express.Router();
 
+// GET all tasks
+router.get('/', function (req, res) {
+    Task.find()
+        .populate('user', ['username'])
+        .then((tasks) => res.status(200).json({ tasks: tasks }))
+});
+
 module.exports = router;
