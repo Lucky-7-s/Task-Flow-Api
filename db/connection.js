d9b5d5c781235db2466fa69561968460619e2c8d
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 11:49:53 -0600

    heroku api deploy

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_db', { useNewUrlParser: true });
+let mongoURI = '';
+
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = 'mongodb://localhost/users_db';
+}
+
+mongoose.connect(mongoURI);
+// mongoose.connect('mongodb://localhost/users_db', { useNewUrlParser: true });
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
+// requiring dependencies
 require('dotenv').config();
 const express = require('express');
 const userController = require('./controllers/userController');
 const taskController = require('./controllers/taskController');
 
-const PORT = process.env.PORT;
+// defining port
+// const PORT = process.env.PORT;
 
 const logger = require('morgan');
 const cors = require('cors');
 
+// Creating express app
 const app = express();
 
 app.use(express.json());
+// logger middleware
 app.use(logger('dev'));
 app.use(cors());
 
+// app routes
 app.use('/user', userController);
 app.use('/tasks', taskController);
 
-app.listen(PORT, () => {
-    console.log(`Listening to port: ${PORT}`);
+// start app on port
+app.set('port', process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
+
+// app.listen(PORT, () => {
+//     console.log(`Listening to port: ${PORT}`);
+// });
