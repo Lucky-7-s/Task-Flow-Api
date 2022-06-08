93e767bda3e721617ec90d7f5eb5286e59d17f37
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-08 10:38:04 -0600

    add create new task route

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -10,4 +10,14 @@ router.get('/', function (req, res) {
         .then((tasks) => res.status(200).json({ tasks: tasks }))
 });
 
+// POST create new task
+router.post('/', function (req, res) {
+    const data = req.body;
+
+    Task.create(data)
+        .then((user) => res.status(201).json({
+            user: user
+        }));
+});
+
 module.exports = router;
