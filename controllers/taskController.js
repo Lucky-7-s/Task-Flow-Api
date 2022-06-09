c78d19c23cbd4898767480e82399d35fc2d74df9
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-09 14:16:41 -0600

    updated routes

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,36 +5,60 @@ const router = express.Router();
 
 // GET all tasks
 router.get('/', function (req, res) {
-    Task.find()
-        .populate('user', ['username'])
-        .then((tasks) => res.status(200).json({ tasks: tasks }))
+    Task.find({})
+        .then((tasks) => {
+            res.json(tasks);
+        });
 });
 
-// POST create new task
+// POST - create a task
 router.post('/', function (req, res) {
-    const data = req.body;
-
-    Task.create(data)
-        .then((user) => res.status(201).json({
-            user: user
-        }));
+    Task.create(req.body)
+        .then((task) => {
+            res.json(task)
+        });
 });
 
 // DELETE task by id
-router.delete('/task/:id', function (req, res) {
-    const id = req.params.id;
-
-    Task.findByIdAndDelete(id)
-        .then(() => res.sendStatus(204));
+router.delete('/:id', function (req, res) {
+    Task.findByIdAndDelete(req.params.id)
+        .then((task) => {
+            res.json(task)
+        });
 });
 
-// PATCH update task by id
-router.patch('/task/:id', function (req, res) {
-    const id = req.params.id;
-    const data = req.body;
+// GET all tasks
+// router.get('/', function (req, res) {
+//     Task.find()
+//         .populate('user', ['username'])
+//         .then((tasks) => res.status(200).json({ tasks: tasks }))
+// });
 
-    Task.findByIdAndUpdate(id, data, { new: true })
-        .then((task) => res.status(200).json({ task: task }))
-});
+// // POST create new task
+// router.post('/user/:username', function (req, res) {
+//     const data = req.body;
+
+//     Task.create(data)
+//         .then((user) => res.status(201).json({
+//             user: user
+//         }));
+// });
+
+// // DELETE task by id
+// router.delete('/:id', function (req, res) {
+//     const id = req.params.id;
+
+//     Task.findByIdAndDelete(id)
+//         .then(() => res.sendStatus(204));
+// });
+
+// // PATCH update task by id
+// router.patch('/task/:id', function (req, res) {
+//     const id = req.params.id;
+//     const data = req.body;
+
+//     Task.findByIdAndUpdate(id, data, { new: true })
+//         .then((task) => res.status(200).json({ task: task }))
+// });
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,54 +1,126 @@
 const express = require('express');
 const User = require('../models/userSchema');
-const Task = require('../models/taskSchema');
 
 const router = express.Router();
 
 // GET all users
 router.get('/', function (req, res) {
-    User.find()
-        .populate('tasks', ['tasks', 'status'])
-        .then((users) => res.status(200).json({ user: users }))
-});
-
-// GET user by username
-router.get('/:user', function (req, res) {
-    User.find({ username: req.params.user })
-        .populate('tasks', ['task', 'status'])
-        .then((user) => res.status(200).json({ user: user }))
-});
-
-// POST create new username
-router.post('/', function (req, res) {
-    User.create(req.body).then((user) => {
-        res.status(201).json({ user: user })
+    User.find({}).then((authors) => {
+        res.json(authors);
     });
 });
 
-// DELETE user by username
-router.delete('/username/:user', function (req, res) {
-    User.findOneAndDelete({ username: req.params.user })
-        .then((user) => res.status(200).json({ user: user }));
+// GET user by username
+router.get('/:username', function (req, res) {
+    User.find({ username: req.params.username }).then((users) => {
+        res.json(users);
+    });
 });
 
-router.patch('/:userId/tasks/:taskId', (req, res) => {
-    const taskId = req.params.taskId;
-    const userId = req.params.userId;
-    const status = req.body.status;
+// POST - create a new username
+router.post('/', function (req, res) {
+    const data = req.body;
 
-	Task.findByIdAndUpdate(taskId, {
-        user: userId,
-        status: status
-    }, { new: true }
-    ).then(() => {
-        User.findByIdAndUpdate(userId, {
-            $push: { tasks: taskId }
-        }, {
-            new: true
-        })
-        .populate('tasks', ['task', 'status'])
-        .then((user) => res.status(200).json({ user: user }))
-    })
+   User.create(data).then((user) => {
+       res.json(user);
+   });
 });
+
+// GET user and tasks
+router.get('/:id/tasks', (req, res) => {
+    User.find({ _id: req.params.id })
+        .populate('tasks')
+        .then((users) => {
+            res.json(users);
+        });
+});
+
+// // GET all users
+// router.get('/', function (req, res) {
+//     User.find()
+//         .populate('tasks', ['tasks', 'status'])
+//         .then((users) => res.status(200).json({ user: users }))
+// });
+
+// // GET user by username
+// router.get('/:user', function (req, res) {
+//     User.find({ username: req.params.user })
+//         .populate('tasks', ['task', 'status'])
+//         .then((user) => res.status(200).json({ user: user }))
+// });
+
+// // POST create new username
+// router.post('/', function (req, res) {
+//     const data = req.body;
+
+//     Task.create(data)
+//         .then((user) => res.status(201).json({
+//             user: user
+//         }));
+
+//     User.create(req.body).then((user) => {
+//         res.status(201).json({ user: user })
+//     });
+// });
+
+// router.post('/:user', function (req, res) {
+//     const username = req.params.user;
+//     const data = req.body;
+
+//     Task.create(data)
+//         .then((user) => res.status(201).json({
+//             user: user
+//         }))
+//         .then(() => {
+//             User.findOneAndUpdate(username, {
+//                 $push: { tasks: data }
+//             }, { new: true })
+//                 .populate('tasks', ['task', 'status'])
+//                 .then((user) => res.status(200).json({ user: user }))
+//         })
+// });
+
+// // DELETE user by username
+// router.delete('/username/:user', function (req, res) {
+//     User.findOneAndDelete({ username: req.params.user })
+//         .then((user) => res.status(200).json({ user: user }));
+// });
+
+// router.patch('/:userId/tasks/:taskId', (req, res) => {
+//     const taskId = req.params.taskId;
+//     const userId = req.params.userId;
+//     const status = req.body.status;
+
+//     Task.findByIdAndUpdate(taskId, {
+//         user: userId,
+//         status: status
+//     }, { new: true }
+//     ).then(() => {
+//         User.findByIdAndUpdate(userId, {
+//             $push: { tasks: taskId }
+//         }, {
+//             new: true
+//         })
+//             .populate('tasks', ['task', 'status'])
+//             .then((user) => res.status(200).json({ user: user }))
+//     });
+// });
+
+// router.post('/:username', function (req, res) {
+//     const data = req.body;
+//     const status = req.body.status;
+
+//     Task.create(data)
+//         .then((user) => res.status(201).json({
+//             user: user,
+//             status: status
+//         })).then(() => {
+//             User.findOneAndUpdate(req.params.username, {
+//                 $push: { tasks: data }
+//             }, { new: true })
+//                 .populate('tasks', ['task', 'status'])
+//                 .then((user) => res.status(200).json({ user: user }))
+//         })
+// });
 
 module.exports = router;
